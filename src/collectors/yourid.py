3bbc385b6aef50238b86e146028ee2dfe1d0ee63
heitorgentil <heitor_albuquerque@yahoo.com.br>
2026-09-05 23:13:34 -0300

    inclusão de imagens e ajuste de retorno de informação (print/return)

--- src/collectors/yourid.py
+++ src/collectors/yourid.py
@@ -1,73 +1,77 @@
 import requests
 import pandas as pd
 
-url = "https://www.youridstore.com.br/api"
 
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept": "application/json, text/plain, */*",
-    "Content-Type": "application/json",
-    "Origin": "https://www.youridstore.com.br",
-    "Referer": "https://www.youridstore.com.br/"
-}
+def coletar_tenis_yourid():
 
-tenis_encontrados = []
+    url = "https://www.youridstore.com.br/api"
 
-pagina = 1
-
-while True:
-
-    payload = {
-        "json": None,
-        "params": f"?page={pagina}&pageSize=24&textLink=sale&showProductGroupingFilter=true",
-        "request": "111d873e-0fb6-4e48-88af-082c64b65e66/198bca71-8b1e-48d8-947f-79baa37b8fa7"
+    headers = {
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "Origin": "https://www.youridstore.com.br",
+        "Referer": "https://www.youridstore.com.br/"
     }
 
-    response = requests.post(
-        url,
-        json=payload,
-        headers=headers
-    )
+    tenis_encontrados = []
+    pagina = 1
 
-    dados = response.json()
-    produtos = dados["products"]
-    
+    while True:
 
-    if len(produtos) == 0:
-        break
+        payload = {
+            "json": None,
+            "params": f"?page={pagina}&pageSize=24&textLink=sale&showProductGroupingFilter=true",
+            "request": "111d873e-0fb6-4e48-88af-082c64b65e66/198bca71-8b1e-48d8-947f-79baa37b8fa7"
+        }
 
-    print(f"\nPágina {pagina}")
+        response = requests.post(
+            url,
+            json=payload,
+            headers=headers
+        )
 
-    for produto in produtos:
+        dados = response.json()
+        produtos = dados["products"]
 
-        if "skUs" not in produto:
-            continue
+        if len(produtos) == 0:
+            break
 
-        skus = produto["skUs"]
+        print(f"\nPágina {pagina}")
 
-        for sku in skus:
-            tamanho = sku["variations"][0]["name"]
-            estoque = sku["stock"]
+        for produto in produtos:
 
-            if "Tênis" in produto["name"] and tamanho == "41" and estoque > 0:
+            if "skUs" not in produto:
+                continue
 
-                tenis = {
-                    "id_produto": produto["idProduct"],
-                    "nome": produto["name"],
-                    "marca": produto["brand"]["name"],
-                    "preco_original": produto["price"],
-                    "preco_promocional": produto["pricePromotion"],
-                    "tamanho": tamanho,
-                    "estoque": estoque,
-                    "link": f"https://www.youridstore.com.br/{produto['urlFriendly']}?sku={sku['idSku']}"
-                }
+            skus = produto["skUs"]
 
-    tenis_encontrados.append(tenis)
+            for sku in skus:
 
-    pagina = pagina + 1
+                tamanho = sku["variations"][0]["name"]
+                estoque = sku["stock"]
 
-df = pd.DataFrame(tenis_encontrados)
-df = df.drop_duplicates(subset=["id_produto"])
-df["desconto_pct"] = ((1-(df["preco_promocional"] / df["preco_original"]))*100).round(0)
-print(df)
+                if "Tênis" in produto["name"] and tamanho == "41" and estoque > 0:
+
+                    tenis = {
+                        "id_produto": produto["idProduct"],
+                        "nome": produto["name"],
+                        "marca": produto["brand"]["name"],
+                        "preco_original": produto["price"],
+                        "preco_promocional": produto["pricePromotion"],
+                        "tamanho": tamanho,
+                        "estoque": estoque,
+                        "imagem": "https:" + produto["imageHome"],
+                        "link": f"https://www.youridstore.com.br/{produto['urlFriendly']}?sku={sku['idSku']}"
+                    }
+
+                    tenis_encontrados.append(tenis)
+
+        pagina = pagina + 1
+
+    df = pd.DataFrame(tenis_encontrados)
+    df = df.drop_duplicates(subset=["id_produto"])
+    df["desconto_pct"] = ((1 - (df["preco_promocional"] / df["preco_original"])) * 100).round(0)
+
+    return df
 
